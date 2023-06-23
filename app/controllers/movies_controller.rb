ff916b169afccb340288b57f986aba73ab400937
George Samuel <geosam2708@gmail.com>
2023-06-24 02:38:33 +0300

    Completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,7 @@
 class MoviesController < ApplicationController
+  rescue_from  ActiveRecord::RecordNotFound, with: :record_not_found_response
+  
+  wrap_parameters format: []
   
   def index
     movies = Movie.all
@@ -9,10 +12,31 @@ class MoviesController < ApplicationController
     render json: Movie.create(movie_params), status: :created
   end
 
+  def update
+    movie = find_movie
+    movie.update(movie_params)
+    render json: movie, status: :accepted
+  end
+
+  def destroy
+    movie = find_movie
+    movie.destroy
+    head :no_content
+  end
+
+
   private
 
   def movie_params
     params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
+  def find_movie
+    Movie.find(id: params[:id])
+  end
+
+  def record_not_found_response
+    render json: {error: "Movie not found"}, status:  :not_found
   end
 
 end
